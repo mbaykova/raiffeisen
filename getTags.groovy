d3b424ff51f898c74964910af67ec0373bcc3f3a
MBaykova <Mariya.Baykova@aplana.com>
2019-09-10 21:46:58 +0300

    run for docker

--- getTags.groovy
+++ getTags.groovy
@@ -23,6 +23,7 @@ def getTags()
     assert object instanceof Map
     String[] tags = object.items.testCaseKey
     tags.collect { "@$it" }.join(',')
+    def tagsString = tags.toString();
 
 }
-return tags.toString();
+return tagsString;
