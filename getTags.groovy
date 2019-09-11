6dfc5397937e288fea0d8d2b12900114f30e0e43
MBaykova <Mariya.Baykova@aplana.com>
2019-09-11 13:04:50 +0300

    run for docker

--- getTags.groovy
+++ getTags.groovy
@@ -1,52 +1,62 @@
 import groovy.json.JsonParserType
 import groovy.json.JsonSlurper
 
+import javax.net.ssl.KeyManager
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLEngine
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509ExtendedTrustManager
+import java.security.cert.X509Certificate
+
+@NonCPS
 def getTags()
 {
-    @NonCPS
-    def nullTrustManager = [
-            @NonCPS
-            checkClientTrusted: { chain, authType ->  },
-            checkServerTrusted: { chain, authType ->  },
-            getAcceptedIssuers: { null }
-    ]
+    TrustManager trustAllCert =   new X509ExtendedTrustManager() {
+        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+        public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
+        public void checkClientTrusted(X509Certificate[] certs, String authType, SSLEngine engine ) {  }
+        public void checkClientTrusted(X509Certificate[] certs, String authType, Socket engine ) {  }
+        public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
+        public void checkServerTrusted(X509Certificate[] certs, String authType, Socket engine ) {  }
+        public void checkServerTrusted(X509Certificate[] certs, String authType, SSLEngine engine ) {  }
+    }
 
-    @NonCPS
-    def nullHostnameVerifier = [
-            verify: { hostname, session ->
-                //true
-                hostname.startsWith('yuml.me')
-            }
-    ]
+    TrustManager[] trustAllCerts = new TrustManager[1];
+    trustAllCerts[0] = trustAllCert;
+    try {
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(new KeyManager[0], trustAllCerts, new java.security.SecureRandom());
 
-    javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext.getInstance("SSL")
-    sc.init(null, [nullTrustManager as  javax.net.ssl.X509TrustManager] as  javax.net.ssl.X509TrustManager[], null)
-    javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
-    javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(nullHostnameVerifier as javax.net.ssl.HostnameVerifier)
-
-
-    def url = new URL('https://mysecureserver')
-    url.openStream()
-    def jiraHost = "http://localhost:8087"
-    def testRunKey = "TEST-C1"
-    def username = "admin"
-    def password = "admin"
-    def baseURL = "${jiraHost}/rest/atm/1.0/testrun/${testRunKey}";
+        url.openStream()
+        def jiraHost = "http://localhost:8087"
+        def testRunKey = "TEST-C1"
+        def username = "admin"
+        def password = "admin"
+        def baseURL = "${jiraHost}/rest/atm/1.0/testrun/${testRunKey}";
 
 
-    url = new URL(baseURL);
+        url = new URL(baseURL);
 
-    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestMethod("GET");
-    connection.doOutput = true
-    connection.addRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=")
-    connection.getResponseCode()
-    def response = connection.getInputStream().getText()
-    def jsonSlurper = new groovy.json.JsonSlurper(type: JsonParserType.INDEX_OVERLAY)
-    def object = jsonSlurper.parseText(response);
-    assert object instanceof Map
-    String[] tags = object.items.testCaseKey
-    return tags.collect { "@$it" }.join(',');
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setSSLSocketFactory(sc.getSocketFactory())
+        connection.doOutput = true
+        connection.addRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=")
+        connection.getResponseCode()
+        def response = connection.getInputStream().getText()
+        def jsonSlurper = new groovy.json.JsonSlurper(type: JsonParserType.INDEX_OVERLAY)
+        def object = jsonSlurper.parseText(response);
+        assert object instanceof Map
+
+        sc.getSocketFactory()
+        String[] tags = object.items.testCaseKey
+        return tags.collect { "@$it" }.join(',');
+    } catch (Exception e) { }
+
+
+
 
 }
 return this;
